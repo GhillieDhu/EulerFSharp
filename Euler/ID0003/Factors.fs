ef5a20da39b001b1fba406b3168d8e536dcd37a1
Mark Phariss <mark.phariss@live.com>
2017-03-31 12:42:28 -0700

    Problem #3: Short circuited the mod evaluation once the primes are too big; massive speedup

--- Euler/ID0003/Factors.fs
+++ Euler/ID0003/Factors.fs
@@ -2,10 +2,12 @@
 
 let quotient (value, candidateFactors) =
     let rec divide dividend divisor divisors =
-        match dividend, dividend % divisor with
-        | 1L, _ -> None
-        | _, 0L -> Some (divisor, (dividend / divisor, divisors))
-        | _, _ -> divide dividend (Seq.head divisors) (Seq.tail divisors)
+        printfn "%d, %d" dividend divisor
+        match dividend, divisor * divisor > dividend, dividend % divisor with
+        | 1L, _, _ -> None
+        | _, true, _ -> Some (dividend, (1L, divisors))
+        | _, _, 0L -> Some (divisor, (dividend / divisor, divisors))
+        | _, _, _ -> divide dividend (Seq.head divisors) (Seq.tail divisors)
     divide value (Seq.head candidateFactors) (Seq.tail candidateFactors)
 
 let factors primes value =
