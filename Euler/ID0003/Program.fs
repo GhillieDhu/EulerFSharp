63d49bc8bf8b3c33eeb3d087e43928c9c339b59a
Mark Phariss <mark.phariss@live.com>
2017-03-31 12:16:19 -0700

    Added timing to Program main

--- Euler/ID0003/Program.fs
+++ Euler/ID0003/Program.fs
@@ -6,8 +6,11 @@ open Primes
 
 [<EntryPoint>]
 let main argv =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let primes = Seq.cache (primes ())
     factors primes 600851475143L
     |> Seq.max
     |> printfn "%A"
+    stopWatch.Stop()
+    printfn "%A" stopWatch.Elapsed
     0 // return an integer exit code
