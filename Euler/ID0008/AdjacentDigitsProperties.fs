e181f6c57dae48893580b6b2439750b20ceb8752
Mark Phariss <mark.phariss@live.com>
2017-04-05 13:50:49 -0700

    Problem #8 Initial error due to integer overflow upon multiplication (not encountered during test!)

---
+++ Euler/ID0008/AdjacentDigitsProperties.fs
@@ -0,0 +1,8 @@
+﻿module AdjacentDigitsProperties
+
+open AdjacentDigits
+open FsCheck.Xunit
+
+[<Property>]
+let ``largest product of four adjacent digits is 5832`` () =
+    largestProduct 4 = 5832L
---
+++ Euler/ID0008/Program.fs
@@ -0,0 +1,15 @@
+﻿open System.Diagnostics
+
+open AdjacentDigits
+
+let solution () = 
+    largestProduct 13
+    |> printfn "Problem 8: %d"
+
+[<EntryPoint>]
+let main argv =
+    let stopWatch = Stopwatch.StartNew()
+    solution ()
+    stopWatch.Stop()
+    printfn "%A" stopWatch.Elapsed
+    0 // return an integer exit code
