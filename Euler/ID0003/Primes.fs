7745f79f7f91e7fba4f0b8cdccb18d17d1a05b05
Mark Phariss <mark.phariss@live.com>
2017-04-07 11:44:46 -0700

    Inverted the test in the isPrime inner function; 4x speedup on the intermediate case (200,000 instead of 2,000,000)

--- Euler/ID0003/Primes.fs
+++ Euler/ID0003/Primes.fs
@@ -3,12 +3,14 @@
 let nextPrime (lastPrime, priorPrimes) =
     let isPrime candidate =
         priorPrimes
-        |> List.filter (fun prime -> prime * prime <= candidate)
-        |> List.forall (fun prime -> candidate % prime > 0L)
+        |> List.exists (fun prime -> candidate % prime = 0L)
+        |> not
+//        |> List.filter (fun prime -> prime * prime <= candidate)
+//        |> List.forall (fun prime -> candidate % prime > 0L)
     let firstCandidate = lastPrime + 1L
     Seq.initInfinite (int64 >> (+) firstCandidate)
     |> Seq.find isPrime
-    |> fun candidate -> Some (candidate, (candidate, candidate :: priorPrimes))
+    |> fun candidate -> Some (candidate, (candidate, List.append priorPrimes [candidate]))
 
 let primes () =
     Seq.unfold nextPrime (1L, [])
