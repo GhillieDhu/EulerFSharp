5e18d1b1ed734288e5a6c7bb6eb617b61123bad6
Mark Phariss <mark.phariss@live.com>
2017-04-07 15:51:31 -0700

    Profiler indicated that append was taking 98% of the time; refactored to stop passing the full list of known primes as part of the state for unfold (requires naive list of potential divisors). <10 s!

--- Euler/ID0003/Primes.fs
+++ Euler/ID0003/Primes.fs
@@ -1,18 +1,29 @@
 ﻿module Primes
 
-let nextPrime (lastPrime, priorPrimes) =
+let multiplesOf n =
+    Seq.initInfinite ((+) 2 >> (*) n)
+
+let divisors =
+    seq {
+        yield 2
+        yield! Seq.initInfinite ((*) 2 >> (+) 3)
+    }
+    |> Seq.cache
+
+let nextPrime lastPrime =
     let isPrime candidate =
-        priorPrimes
-        |> List.exists (fun prime -> candidate % prime = 0)
+        divisors
+        |> Seq.takeWhile (fun prime -> prime * prime <= candidate)
+        |> Seq.exists (fun prime -> candidate % prime = 0)
         |> not
     let firstCandidate = lastPrime + 2
     Seq.initInfinite ((*) 2 >> (+) firstCandidate)
     |> Seq.find isPrime
-    |> fun candidate -> Some (candidate, (candidate, List.append priorPrimes [candidate]))
+    |> fun candidate -> Some (candidate, candidate)
 
 let primes () =
     seq {
         yield 2
         yield 3
-        yield! Seq.unfold nextPrime (3, [2; 3])
+        yield! Seq.unfold nextPrime 3
     }
