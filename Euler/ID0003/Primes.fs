fe4675c1506a07342716969ddc50294aeb4c2ee4
Mark Phariss <mark.phariss@live.com>
2017-04-09 17:01:51 -0700

    Cleanup of commented-out code

--- Euler/ID0003/Primes.fs
+++ Euler/ID0003/Primes.fs
@@ -1,13 +1,10 @@
 ﻿module Primes
 
 let primes =
-//    let divisors n =
-//        [3..2..(int (sqrt (float n)))]
     let nextPrime lastPrime =
         let isPrime candidate =
             (candidate % 2 = 1)
             &&
-//            [3..2..(int (sqrt (float candidate)))]
             seq {for i in 3 ..2 .. (int (sqrt (float candidate))) -> i}
             |> Seq.exists (fun prime -> candidate % prime = 0)
             |> not
