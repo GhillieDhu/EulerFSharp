4192faf6b677ba8cab679f70ff20481d4f4559df
Mark Phariss <mark.phariss@live.com>
2017-04-13 12:53:38 -0700

    Exploited the indexed access of arrays to refactor into Array-only implementation; 6-8x speedup (now a little more than a second).

--- Euler/ID0014/Collatz.fs
+++ Euler/ID0014/Collatz.fs
@@ -8,28 +8,27 @@ let nextCollatz =
 
 let longestCollatz n =
     let N = uint64 n
-    let rec terminus k steps i =
+    let rec terminus steps i =
         let next = nextCollatz i
         if next < N
-        then (k, (next, steps))
-        else terminus k (steps+1) next
+        then (int next, steps)
+        else terminus (steps+1) next
     let segments =
-        Array.init (n-1) (fun i -> terminus (uint64 i + 1UL) 1 (uint64 i + 1UL))
-        |> Map.ofArray
-    let rec dereference partials =
-        let chain i (n, s) =
-            if n = 0UL
-            then (n, s)
+        Array.init (n-1) (fun i -> terminus 1 (uint64 i + 1UL))
+    let dereference partials =
+        let rec chain links =
+            if Array.forall (fun (n, s) -> n = 0) links
+            then links
             else
-                let (n', s') = Map.find n partials
-                (n', s + s')
-        let chained =
-            partials
-            |> Map.map chain
-        if Map.forall (fun k (n, s) -> n = 0UL) chained
-        then chained
-        else dereference chained
+                links
+                |> Array.map (fun (n, s) ->
+                    if n = 0
+                    then (n, s)
+                    else
+                        let next = links.[n-1]
+                        (fst next, s + snd next))
+                |> chain
+        chain partials
     dereference segments
-    |> Map.toSeq
     |> Seq.maxBy snd
     |> fst
