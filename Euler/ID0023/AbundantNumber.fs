40f1a1d8bafc75fd978a509615156bc85c4ae2e2
Mark Phariss <mark.phariss@live.com>
2017-04-17 08:14:23 -0700

    Some fiddling with array bounds, but down to barely over a second.

--- Euler/ID0023/AbundantNumber.fs
+++ Euler/ID0023/AbundantNumber.fs
@@ -3,39 +3,17 @@
 open Factors
 
 let areAbundant n =
-    Array.init n (uint64 >> (+) 1UL >> fun i -> (i, Seq.sum (allFactors i) > 2UL*i))
-//    Seq.init n (uint64 >> (+) 1UL)
-//    |> Seq.choose (fun i ->
-//        if Seq.sum (allFactors i) > 2UL*i
-//        then Some i
-//        else None)
+    Array.init (n-1) (uint64 >> (+) 1UL >> fun i -> (i, Seq.sum (allFactors i) > 2UL*i))
 
 let nonAbundantSumsBelow n =
     let abundance =
         areAbundant (int n)
     let abundants =
         abundance
-        |> Array.choose (fun (i, isAbundant) -> if isAbundant then Some i else None)
+        |> Array.choose (fun (i, isAbundant) -> if isAbundant then Some (i+1UL) else None)
     let candidates = Array.init (n-1) (uint64 >> (+) 1UL)
-    let sums =
-        candidates
-        |> Array.map (fun i ->
-            (i, Array.exists (fun a -> i > a && snd (Array.get abundance (int (i-a)))) abundants))
-        |> Array.choose (fun (i, abundantPairSum) -> if abundantPairSum then None else Some i)
-        |> Array.sum
-//    let sums =
-//        Seq.allPairs abundants abundants
-//        |> Seq.choose (fun (a, b) ->
-//            if a < b
-//            then None
-//            else
-//                let c = a + b
-//                if c < n
-//                then Some c
-//                else None)
-//        |> Seq.distinct
-//        |> Seq.sum
-//    let possibleSum =
-//        (n * (n-1UL))/2UL
-//    possibleSum - sums
-    sums
+    candidates
+    |> Array.map (fun i ->
+        (i, Array.exists (fun a -> i >= a && snd (Array.get abundance (int (i-a)))) abundants))
+    |> Array.choose (fun (i, abundantPairSum) -> if abundantPairSum then None else Some i)
+    |> Array.sum
