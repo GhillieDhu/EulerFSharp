c776c068780ae2aefb253696f565b4a44f134ee6
Mark Phariss <mark.phariss@live.com>
2017-04-24 17:21:38 -0700

    Added (not-really-property-based) test to link search algorithm to previously-tested enumeration algorithm

--- Euler/ID0024/PermutationProperties.fs
+++ Euler/ID0024/PermutationProperties.fs
@@ -2,9 +2,24 @@
 
 open Permutation
 open FsCheck.Xunit
+open System
 
 [<Property>]
 let ``lex perms of 0,1,2`` () =
     lexicographic 3
     |> Seq.zip (Seq.ofList ["012";"021";"102";"120";"201";"210"])
     |> Seq.forall (fun (a, b) -> a = b)
+
+[<Property>]
+let ``nth lex matches enumeration of lex`` () =
+    let lex =
+        lexicographic 3
+        |> Array.ofSeq
+    let elems = List.init 3 id
+    Array.mapi (fun i perm ->
+        perm =
+            (nthLexicographic elems (i+1)
+            |> Seq.map string
+            |> Seq.fold (+) String.Empty)) lex
+    |> Array.forall id
+    
