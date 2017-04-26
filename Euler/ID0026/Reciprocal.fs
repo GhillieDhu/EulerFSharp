453e546d0be93c66b9b14b9022f10a5ba70ca971
Mark Phariss <mark.phariss@live.com>
2017-04-26 11:01:53 -0700

    Renaming

--- Euler/ID0026/Reciprocal.fs
+++ Euler/ID0026/Reciprocal.fs
@@ -6,16 +6,16 @@ open System.Numerics
 
 let primeRecipCyc =
     let ten = BigInteger 10
-    let rec unfloat n d =
+    let rec countNines n d =
         let composite = (ten**d) - BigInteger.One
         if composite % n = BigInteger.Zero
         then d
-        else unfloat n (d+1)
+        else countNines n (d+1)
     primes
     |> Seq.map (fun p ->
         match p with
         | 2UL | 5UL -> (p, 0)
-        | _ -> (p, unfloat (BigInteger p) 1))
+        | _ -> (p, countNines (BigInteger p) 1))
     |> Seq.cache
 
 let recipCyc n =
